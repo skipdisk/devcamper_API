cff67ea4ac6f894803f0dc92c4e709aacb2c7fae
Thien Nguyen <Thiennguyenwsb@gmail.com>
2020-02-01 06:48:51 -0800

    MongoDB connection to application

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
 
+const logger = require('./middlewares/logger')
+const connectDB = require('./config/db')
+
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
+//load env variables
 dotenv.config({
     path: './config/config.env'
-});
+})
+
+//Connect to database
+connectDB()
 
 const app = express()
 
-const PORT = process.env.PORT || 5000;
+//uses logging middleware for development mode
+if (process.env.NODE_ENV === 'development') {
+    //app.use(logger);
+    app.use(morgan('dev'))
+}
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+//handle unhandle promise rejections
+
+process.on('unhandleRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => { //URI stored inn config.env
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+
+    console.log(`MongpDb Connected: ${conn.connection.host}`)
+}
+
+module.exports = connectDB
